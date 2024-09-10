3a81cf8e2d2d400defb8a19290071198836fe205
Shirjoy Bhuban <50661137+shirjoybhuban@users.noreply.github.com>
2024-09-10 15:43:41 +0600

    project initialize

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import './App.css';
+import LoginComponent from './components/auth/LoginComponent';
+
+function App() {
+  return (
+    <div className="App">
+      <LoginComponent/>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/auth/LoginComponent/index.tsx
@@ -0,0 +1,11 @@
+import LoginForm from "./LoginForm";
+import UserCard from "./UserCard";
+
+const LoginComponent = () => {
+  return <>
+    <div><LoginForm /></div>
+    <div><UserCard /></div>
+  </>;
+};
+
+export default LoginComponent;
