53924697386874ce4c855802617a6196f530f35f
Shirjoy Bhuban <50661137+shirjoybhuban@users.noreply.github.com>
2024-09-10 21:54:44 +0600

    Persist State

--- src/context/AuthContext.tsx
+++ src/context/AuthContext.tsx
@@ -1,4 +1,4 @@
-import React, { createContext, useState } from 'react';
+import React, { createContext, useEffect, useState } from 'react';
 import { UserContextType } from '../types/authType';
 
 // Create the context
@@ -7,7 +7,16 @@ export const AuthContext = createContext<UserContextType | undefined>(undefined)
 //Provider component
 export const AuthProvider = ({ children }: any) => {
   const [user, setUser] = useState(null);
+  const [isHydrate, setIsHydrate] = useState(false);
 
+  useEffect(() => {
+    let data : string | null = localStorage.getItem('auth-user');
+    if(data && data?.length > 0){
+      setUser(JSON.parse(data));
+    }
+    setIsHydrate(true);
+  }, []);
+  
   const handleUser = (user: any) => {
     setUser(user);
     localStorage.setItem('auth-user',JSON.stringify(user));
@@ -17,6 +26,10 @@ export const AuthProvider = ({ children }: any) => {
     setUser(null);
     localStorage.removeItem('auth-user');
   };
+
+  if(!isHydrate){
+    return <p>Initializing...</p>
+  }
 
   return (
     <AuthContext.Provider value={{ user, handleUser, logout }}>
